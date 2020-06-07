9c3b69d70be1a6df85dc117f9fb5326f6b69940e
Nancy Golden <pngolden@verizon.net>
2020-06-07 13:36:10 -0500

    tweaked files

--- src/DoStuffToList.jsx
+++ src/DoStuffToList.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from "react"; //react is the library
-import ViewList from "./ViewList.jsx"; //path to ViewList.jsx file
-// import ButtonSelect from "./ButtonSelect.jsx"; //function name must match
+import React, { useState } from "react"; 
+import ViewList from "./ViewList"; 
 import AddTodo from "./AddTodo.jsx";
-//In this case the name of the function in the ButtonSelect.jsx file is ButtonSelect
+//The name of the function in AddTodo.jsx is AddTodo
 
 function DoStuffToList() {
   
   const [todos, setTodos] = useState(["walk the dog", "feed the fish"]);
-
-  //placed here so it is used inside the
+  
+  //Code placed here so it is used inside the
   //function. useState is a storage container that uses these parameters - the first one
-  // is a variable and the second one is a function to change the variable. This is the
-  //only way we can change the state which is list. Default value is an empty string - every
-  //time " " (list) changes - must use ViewList which is a function and is
+  // todos is a variable and second one setTodos is a function to change the variable. This is the
+  //only way we can change the state which is "list". Default value in this case is
+  //"walk the dog", "feed the fish" for testing purposes - usually would be [] (empty array).
+  // Every time " " (list) changes - must use setTodos which is a function and is
   //the 2nd parameter.
 
   const addTodo = input => {
-    setTodos([...todos, input])
-    // Child display is being imported from ButtonSelect function
-    //in ButtonSelect.jsx This brings buttonpad click child data to DoStuffToList parent
+    setTodos([...todos, input])//...todos
+    // Child is being imported from AddTodo function
+    //in AddTodo.jsx This brings input child data to DoStuffToList parent
   };
   const deletetodo = index => {
      let filtered = todos.filter((val, ind) => ind!==index); 
@@ -27,9 +27,7 @@ function DoStuffToList() {
   const deleteAll = () => {
     setTodos([])
   };
-  const checkoffitem = (checkofftodo) => {
-    checkofftodo = 4; //placeholder code
-  };
+  
   return (
     <div className="list">
       <AddTodo addTodo={addTodo} deleteAll={deleteAll} /> 
@@ -37,13 +35,8 @@ function DoStuffToList() {
       {/*The className property sets or returns the class name of an element*/}
       <ViewList pizza={todos} deletetodo={deletetodo} />
       {/*{todos} is the useState prop (props.pizza in ViewList.jsx) thats passed to todos*/}
-      {/*<ButtonSelect
-        addtodo={addtodo}
-        deletetodo={deletetodo}
-        deletalltodo={deletealltodo}
-        checkofftodo={checkofftodo}
-      />*/}
+      
     </div>
-  );
-}
+  )
+ }  
 export default DoStuffToList;
--- src/ViewList.jsx
+++ src/ViewList.jsx
@@ -2,17 +2,21 @@ import React, { useState } from "react";
 
 function ViewList(props) {
   return (
-    <div>{props.pizza.map((todo,i)=>
-    <p key={i}>{todo}
-    <button onClick={() => props.deletetodo(i)} >Delete</button>
-    <button>Completed</button>
-    </p>
-      )}</div> 
+    <div>
+      {props.pizza.map((todo, i) => (
+        <p key={i}>
+          {todo}
+          <button onClick={() => props.deletetodo(i)}>Delete</button>
+          <button>Completed</button>
+          <checkbox onClick={() => props.checktodo(i)}>Completed</checkbox>
+          <input type="checkbox"/>
+        </p>
+      ))}</div>
   )
 
   //The className property sets or returns the class name of an element
 
-  //the only way to receive the value of display is through props.todoitems
+  //the only way to receive the value of display is through props.pizza
   //because has to come from parent (DoStuffToList)
 }
 
